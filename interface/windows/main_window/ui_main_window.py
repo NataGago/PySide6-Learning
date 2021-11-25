05388e18c80207176334d5da4aa86ddffe129d03
Nata Gago <natagago123@gmail.com>
2021-11-24 22:10:37 -0300

    Adicionado top bar layout com labels

--- interface/windows/main_window/ui_main_window.py
+++ interface/windows/main_window/ui_main_window.py
@@ -30,16 +30,60 @@ class UI_MainWindow(object):
         # LEFT MENU
         self.left_menu = QFrame()
         self.left_menu.setStyleSheet("background-color: #44475a")
+        self.left_menu.setMinimumWidth(50)
         self.left_menu.setMaximumWidth(50)
         
         # CONTENT
         self.content = QFrame()
         self.content.setStyleSheet("background-color: #282a36")
         
+        # Content Layout
+        self.content_layout = QVBoxLayout(self.content)
+        self.content_layout.setContentsMargins(0, 0, 0, 0)
+        self.content_layout.setSpacing(0)
+        
+        # TOP BAR
+        self.top_bar = QFrame()
+        self.top_bar.setMinimumHeight(30)
+        self.top_bar.setMaximumHeight(30)
+        self.top_bar.setStyleSheet("background-color: #21232d; color: #6272a4")
+        self.top_bar_layout = QHBoxLayout(self.top_bar)
+        self.top_bar_layout.setContentsMargins(10, 0, 10, 0)
+        
+        # Left label
+        self.top_label_left = QLabel("Minha aplicação com PySide6")
+        
+        # Top spacer
+        self.top_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        
+        # Right label
+        self.top_label_right = QLabel("| PÁGINA INICIAL")
+        self.top_label_right.setStyleSheet("font: 700 9pt 'Segoe UI'")
+        
+        # Add to top bar layout
+        self.top_bar_layout.addWidget(self.top_label_left)
+        self.top_bar_layout.addSpacerItem(self.top_spacer)
+        self.top_bar_layout.addWidget(self.top_label_right)
+        
+        # Application pages
+        self.pages = QStackedWidget()
+        self.pages.setStyleSheet("font-size: 12pt; color: #f8f8f2")
+        
+        # BOTTOM BAR
+        self.bottom_bar = QFrame()
+        self.bottom_bar.setMinimumHeight(30)
+        self.bottom_bar.setMaximumHeight(30)
+        self.bottom_bar.setStyleSheet("background-color: #21232d; color: #6272a4")
+        
+        # Add to content layout
+        self.content_layout.addWidget(self.top_bar)
+        self.content_layout.addWidget(self.pages)
+        self.content_layout.addWidget(self.bottom_bar)
+        
+        
         # ADD WIDGETS TO APP
         self.main_layout.addWidget(self.left_menu)
         self.main_layout.addWidget(self.content)
-        
         
         # SET CENTRAL WIDGET
         parent.setCentralWidget(self.central_frame)
